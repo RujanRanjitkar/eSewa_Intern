bc34032c00887b8c204719cedc4881bcf145e455
RujanRanjitkar <ranjitrujan123@gmail.com>
2023-03-22 10:57:34 +0545

    day 4 ruajn

--- src/Day3/OperatorDemo.java
+++ src/Day3/OperatorDemo.java
@@ -1,25 +1,26 @@
 package Day3;
+
 import java.util.Scanner;
+
 public class OperatorDemo {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter Meal price:");
-        float mealprice= sc.nextFloat();
+        float mealPrice = sc.nextFloat();
         System.out.println("Enter tip:");
-        float tip= sc.nextFloat();
+        float tip = sc.nextFloat();
+        tip = tip / 100 * mealPrice;
         System.out.println("Enter tax:");
-        float tax= sc.nextFloat();
-        tax=tax/100*mealprice;
-        if(mealprice>=5000 && mealprice<=10000){
-            tip=tip/100*mealprice;
-            mealprice=mealprice-500;
+        float tax = sc.nextFloat();
+        tax = tax / 100 * mealPrice;
+        if (mealPrice >= 5000 && mealPrice <= 10000) {
+            mealPrice = mealPrice - 500;
+        } else if (mealPrice > 10000) {
+            tip = tip - (float) 20 / 100 * tip;
+            mealPrice = mealPrice - 700;
         }
-        else if(mealprice>10000){
-            tip=(tip/100*mealprice)-((float) 20/100*(tip/100*mealprice));
-            mealprice=mealprice-700;
-        }
-        int total=Math.round(mealprice+tip+tax);
-        System.out.println(mealprice);
+        int total = Math.round(mealPrice + tip + tax);
+        System.out.println(mealPrice);
         System.out.println(tip);
         System.out.println(tax);
         System.out.println("The total cost is:" + total);
---
+++ src/Day4/IfStatement.java
@@ -0,0 +1,12 @@
+package Day4;
+
+public class IfStatement {
+    //basic statement among all control flow statements in Java
+    public static void main(String[] args) {
+        int num1=10;
+        int num2=20;
+        if(num1+num2>=25){
+            System.out.println("Inside if statement");
+        }
+    }
+}
